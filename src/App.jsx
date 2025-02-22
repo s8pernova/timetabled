674650eae1a0001aff891b623af25349ec8daffe
achrunaway <achrunaway@gmail.com>
2025-02-22 16:00:14 -0500

    add files

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import Calendar from "./components/Calendar";
+
+const App = () => {
+	return (
+		<div className="App">
+			<div>
+				<h1>Itinerary for 7 Days in Chicago 🏙️</h1>
+				<h2>
+					Welcome to Chicago, Natalia! Check out this calendar to get to know
+					the city and see all the sights during your stay.
+				</h2>
+				<Calendar />
+			</div>
+		</div>
+	);
+};
+
+export default App;
---
+++ src/components/Calendar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Calendar = () => {
+	return <div className="Calendar">Testing the calendar</div>;
+};
+
+export default Calendar;
